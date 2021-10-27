8481946ba380be0d73a958bf16f18a2bb634de71
coha <coha@itu.dk>
2021-10-27 15:16:57 +0200

    add Tui to main

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/marcusolsson/tui-go"
+)
+
+type post struct {
+	username string
+	message  string
+	time     string
+}
+
+var posts = []post{
+	{username: "john", message: "hi, what's up?", time: "14:41"},
+	{username: "jane", message: "not much", time: "14:43"},
+}
+var history = tui.NewVBox()
+
+func main() {
+	go runUI()
+	go print()
+	time.Sleep(time.Minute * 5)
+
+}
+func print() {
+	for {
+
+		history.Append(tui.NewHBox(
+			tui.NewLabel(time.Now().Format("15:04")),
+			tui.NewPadder(1, 0, tui.NewLabel(fmt.Sprintf("<%s>", "john"))),
+			tui.NewLabel("EY SPADSER SVAR"),
+			tui.NewSpacer(),
+		))
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func runUI() {
+	for _, m := range posts {
+		history.Append(tui.NewHBox(
+			tui.NewLabel(m.time),
+			tui.NewPadder(1, 0, tui.NewLabel(fmt.Sprintf("<%s>", m.username))),
+			tui.NewLabel(m.message),
+			tui.NewSpacer(),
+		))
+	}
+
+	historyScroll := tui.NewScrollArea(history)
+	historyScroll.SetAutoscrollToBottom(true)
+
+	historyBox := tui.NewVBox(historyScroll)
+	historyBox.SetBorder(true)
+
+	input := tui.NewEntry()
+	input.SetFocused(true)
+	input.SetSizePolicy(tui.Expanding, tui.Maximum)
+
+	inputBox := tui.NewHBox(input)
+	inputBox.SetBorder(true)
+	inputBox.SetSizePolicy(tui.Expanding, tui.Maximum)
+
+	chat := tui.NewVBox(historyBox, inputBox)
+	chat.SetSizePolicy(tui.Expanding, tui.Expanding)
+
+	input.OnSubmit(func(e *tui.Entry) {
+		history.Append(tui.NewHBox(
+			tui.NewLabel(time.Now().Format("15:04")),
+			tui.NewPadder(1, 0, tui.NewLabel(fmt.Sprintf("<%s>", "john"))),
+			tui.NewLabel(e.Text()),
+			tui.NewSpacer(),
+		))
+		input.SetText("")
+	})
+
+	ui, err := tui.New(chat)
+
+	//Setup error handling and a exit funtion
+	if err != nil {
+		log.Fatal(err)
+	}
+	ui.SetKeybinding("Esc", func() { ui.Quit() })
+	if err := ui.Run(); err != nil {
+		log.Fatal(err)
+	}
+
+}
